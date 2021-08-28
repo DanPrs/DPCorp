be2f29e6bba701efa436c5028fd87fbc30b7a1b0
DanPrs <dannyjpires@gmail.com>
2021-08-27 22:15:28 -0400

    comit

--- test/variables.tf
+++ test/variables.tf
@@ -1,15 +1,32 @@
-variable "region" {
-  description = "AWS region"
-  default     = "us-west-1"
+variable "location" {}
+
+variable "admin_username" {
+  type        = string
+  description = "Administrator user name for virtual machine"
 }
 
-variable "instance_type" {
-  description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
+variable "admin_password" {
+  type        = string
+  description = "Password must meet Azure complexity requirements"
 }
 
-variable "instance_name" {
-  description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
+variable "prefix" {
+  type    = string
+  default = "my"
 }
 
+variable "tags" {
+  type = map
+
+  default = {
+    Environment = "Terraform GS"
+    Dept        = "Engineering"
+  }
+}
+
+variable "sku" {
+  default = {
+    westus2 = "16.04-LTS"
+    eastus  = "18.04-LTS"
+  }
+}
