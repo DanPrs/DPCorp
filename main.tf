b075876a2713b840fd7c69b6bae16f5c1cd4ca67
DanPrs <DanPrs@users.noreply.github.com>
2021-09-17 08:39:48 -0400

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Terraform Configuration
 terraform {
   required_providers {
     azurerm = {
@@ -7,6 +8,7 @@ terraform {
   }
 }
 
+# Azure Provider
 provider "azurerm" {
   # Configuration options
   subscription_id = var.subscription_id
@@ -21,4 +23,49 @@ resource "azurerm_resource_group" "RG-VNet-Test" {
   name     = "RG-VNet-Test"
   location = "East US"
 }
-#test
+
+resource "azurerm_network_security_group" "VNet-Prod-NSG01" {
+  name                = "VNet-Prod-NSG01"
+  location            = azurerm_resource_group.RG-VNet-Test.location
+  resource_group_name = azurerm_resource_group.RG-VNet-Test.name
+}
+
+resource "azurerm_network_ddos_protection_plan" "DDOS-Prod" {
+  name                = "DDOS-Prod"
+  location            = azurerm_resource_group.RG-VNet-Test.location
+  resource_group_name = azurerm_resource_group.RG-VNet-Test.name
+}
+
+# vNet
+resource "azurerm_virtual_network" "VNet-Prod" {
+  name                = "VNet-Prod"
+  location            = azurerm_resource_group.RG-VNet-Test.location
+  resource_group_name = azurerm_resource_group.RG-VNet-Test.name
+  address_space       = ["10.10.0.0/16"]
+  dns_servers         = ["1.1.1.1", "1.0.0.1"]
+
+  ddos_protection_plan {
+    id     = azurerm_network_ddos_protection_plan.example.id
+    enable = true
+  }
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.10.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.10.2.0/24"
+  }
+
+  subnet {
+    name           = "subnet3"
+    address_prefix = "10.10.3.0/24"
+    security_group = azurerm_network_security_group.example.id
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,13 +1,24 @@
+# Configured as a variable in the Terraform Cloud portal
 variable "subscription_id" {
   type = string
 }
 
+# Configured as a variable in the Terraform Cloud portal
 variable "client_id" {
   type = string
 }
+
+# Configured as a variable in the Terraform Cloud portal
 variable "client_secret" {
   type = string
 }
+
+# Configured as a variable in the Terraform Cloud portal
 variable "tenant_id" {
   type = string
+}
+
+# Azure default resource location
+variable "location" {
+  default = "East US"
 }
