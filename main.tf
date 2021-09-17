8ce84c72aae3024f2bb8f8a2430ba77a3aaf1df8
DanPrs <DanPrs@users.noreply.github.com>
2021-09-17 14:07:00 -0400

    added Prod VNet

--- main.tf
+++ main.tf
@@ -18,6 +18,7 @@ provider "azurerm" {
   features {}
 }
 
+# Test VNet
 module "VNet-Test" {
   source              = "./vnet"
   environment         = "Test"
@@ -27,4 +28,16 @@ module "VNet-Test" {
   sub1_address_prefix = "10.10.1.0/24"
   sub2_address_prefix = "10.10.2.0/24"
   sub3_address_prefix = "10.10.3.0/24"
+}
+
+# Prod VNet
+module "VNet-Prod" {
+  source              = "./vnet"
+  environment         = "Prod"
+  address_space       = ["10.20.0.0/22"]
+  dns_servers         = ["1.1.1.1", 
+                        "1.0.0.2"]
+  sub1_address_prefix = "10.20.1.0/24"
+  sub2_address_prefix = "10.20.2.0/24"
+  sub3_address_prefix = "10.20.3.0/24"
 }
